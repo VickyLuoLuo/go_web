cd6b940f31ff5e70a7fcfcd142d1acaabacd23e0
luohl0703 <luohl0703@thundersoft.com>
2018-06-13 17:23:22 +0800

    manually parse url params

--- server.go
+++ server.go
@@ -4,16 +4,24 @@ import (
 	"net/http"
 	"fmt"
 	"log"
+	"strings"
 )
 
 func sayhello(w http.ResponseWriter, r *http.Request)  {
 	r.ParseForm()
-	fmt.Println("path:", r.URL.Path)
+	fmt.Println("param_form: ",r.Form)
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("scheme: ", r.URL.Scheme)
+	fmt.Println("one_param_value: ", r.Form["hello"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("value: ",strings.Join(v," "))
+	}
 	fmt.Fprintf(w, "hello go")
 }
 
 func main()  {
-	http.HandleFunc("/hello", sayhello)
+	http.HandleFunc("/", sayhello)
 	err := http.ListenAndServe(":8088", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer: ", err)
